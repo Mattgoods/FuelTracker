804d56d3160ab0cc9d6621142704465988b1e174
Mattgoods <mgoodman624@gmail.com>
2024-04-14 18:58:58 -0400

    Added Home page and Navigation bar

--- app/routes.py
+++ app/routes.py
@@ -4,5 +4,25 @@ from app import create_app
 app = create_app()
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
+
+@app.route('/price_trends')
+def price_trends():
+    # Return price trends page
+    pass
+
+@app.route('/factor_analysis')
+def factor_analysis():
+    # Return factor analysis page
+    pass
+
+@app.route('/regional_comparison')
+def regional_comparison():
+    # Return regional comparison page
+    pass
+
+@app.route('/settings')
+def settings():
+    # Return settings page
+    pass
